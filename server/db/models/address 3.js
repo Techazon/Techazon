209f94ca53d72b54daadfeecd07598cddc6454fb
Matthew Baker <matthewbaker@MacBook-Pro.lan>
2021-06-17 12:00:50 -0400

    add mm association for cart_product

---
+++ server/db/models/address 3.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+const { validate } = require('../db');
+
+const Address = define('address', {
+    
+})
