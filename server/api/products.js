efdb192549f8fb339b41e0450fc86ce3493c6a67
John Lee <lee.john.tk@gmail.com>
2021-06-21 15:22:19 -0500

    feature: add edit and delete products for admin only

--- server/api/products.js
+++ server/api/products.js
@@ -1,11 +1,12 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
   models: { Product, Category },
-} = require("../db");
+} = require('../db');
+const { requireToken, isAdmin } = require('./gatekeepers');
 module.exports = router;
 
 // GET /api/products
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll({
       include: Category,
@@ -16,14 +17,42 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// GET /api/products/:id
+// POST /api/products
+router.post('/', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    res.status(201).send(await Product.create(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get("/:id", async (req, res, next) => {
+// GET /api/products/:id
+router.get('/:id', async (req, res, next) => {
   try {
     const singleProduct = await Product.findByPk(req.params.id, {
       include: Category,
     });
     res.json(singleProduct);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT /api/products/:id
+router.put('/:id', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    const singleProduct = await Product.findByPk(req.params.id);
+    res.send(await singleProduct.update(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE /api/products/:id
+router.delete('/:id', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    const singleProduct = await Product.findByPk(req.params.id);
+    res.send(await singleProduct.destroy());
   } catch (err) {
     next(err);
   }
