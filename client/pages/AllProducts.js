8f842fb981316af3a4903d21f7c34b2600035f24
David Degenstein <ddegenstein15@gmail.com>
2021-06-24 00:06:09 -0400

    Basic styling with flexbox

--- client/pages/AllProducts.js
+++ client/pages/AllProducts.js
@@ -16,62 +16,63 @@ class AllProducts extends React.Component {
   }
 
   render() {
-    const isLoggedIn = localStorage.getItem("token")
+    const isLoggedIn = localStorage.getItem("token");
     const products = this.props.products.products;
     return (
       <div>
         <h2>Check out our retro gear!</h2>
-        {products &&
-          products.map((product) => {
-            return (
-              <div key={product.id}>
-                <img src={product.imageUrl} width="150px" height="150px" />
-                <div>Product Name: {product.productName}</div>
-                <div>Price: ${product.price / 100}</div>
-                {product.stock ? (
-                  <div>Stock: {product.stock}</div>
-                ) : (
-                  <div> SOLD OUT </div>
-                )}
-                {/* <div>{prod.description}</div> */}
-                <div>Category Name: {product.category.categoryName}</div>
-                <button
-                  onClick={() => {
-                    cartFuncs.clickAddToCart(product)
-                    // console.log('WHERE IS CART ID -->', this.props)
-                    console.log('jskhevlkajehf -> ', this.props.cart)
-                    product.cartId = this.props.cart.id
-                    console.log('PROPS CART ID -->', this.props.cart.id)
-                    console.log(isLoggedIn)
-                    isLoggedIn && this.props.addToCart(product)
-                  }}
-                >
-                  Add to Cart
-                </button>
-                <button>Preview</button>
-                <Link to={`/products/${product.id}`}>
-                  <button>View Details</button>
-                </Link>
-                Add Quantity:
-                <select
-                  onChange={(event) => {
-                    product.quantity = event.target.value;
-                  }}
-                >
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                </select>
-              </div>
-            );
-          })}
+        <div id="product-container">
+          {products &&
+            products.map((product) => {
+              return (
+                <div key={product.id} className="product-cell">
+                  <Link to={`/products/${product.id}`}>
+                    <img src={product.imageUrl} />
+                  </Link>
+                  <div className="product-info">
+                    <div><strong>{product.productName}</strong></div>
+                    <div>${product.price / 100}</div>
+                    {!product.stock && <div> SOLD OUT </div>}
+                    <div>{product.description}</div>
+                    {/* <div>Category: {product.category.categoryName}</div> */}
+                  </div>
+                  <div id="button-container">
+                    <button
+                      onClick={() => {
+                        cartFuncs.clickAddToCart(product);
+                        product.cartId = this.props.cart.id;
+                        isLoggedIn && this.props.addToCart(product);
+                      }}
+                    >
+                      Add to Cart
+                    </button>
+                    <Link to={`/products/${product.id}`}>
+                      <button>View Details</button>
+                    </Link>
+                  </div>
+                  <div className="quantity-container">
+                    Add Quantity:
+                    <select
+                      onChange={(event) => {
+                        product.quantity = event.target.value;
+                      }}
+                    >
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                    </select>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
     );
   }
--- client/pages/SingleProduct.js
+++ client/pages/SingleProduct.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchSingleProduct } from '../store/singleProduct';
-import { cartFuncs } from '../helperFuncs';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchSingleProduct } from "../store/singleProduct";
+import { cartFuncs } from "../helperFuncs";
 
 export class SingleProduct extends React.Component {
   constructor(props) {
@@ -13,31 +13,37 @@ export class SingleProduct extends React.Component {
   render() {
     const product = this.props.singleProduct;
     return (
-      <div>
+      <div >
         {product && (
-          <div>
-            <h2>Product Name: {product.productName}</h2>
-            <img src={product.imageUrl} />
-            <p>Price: ${product.price / 100}</p>
-            <p>Description: {product.description}</p>
-            <button onClick={() => cartFuncs.clickAddToCart(product)}>
-              Add to Cart
-            </button>
-            Add Quantity:
-            <select
-              onChange={(event) => cartFuncs.clickQuantity(event, product)}
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
+          <div className="single-product-container">
+            <div className="single-product-image-container">
+              <img src={product.imageUrl} />
+            </div>
+            <div className="single-product-info">
+              <h2>{product.productName}</h2>
+              <em>{product.description}</em>
+              <p>${product.price / 100}</p>
+              <button onClick={() => cartFuncs.clickAddToCart(product)}>
+                Add to Cart
+              </button>
+              <div className='quantity-container'>
+              Add Quantity:
+              <select
+                onChange={(event) => cartFuncs.clickQuantity(event, product)}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+              </div>
+            </div>
           </div>
         )}
       </div>
